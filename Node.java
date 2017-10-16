1c852a5569214f3362c4a3686457badbc55f065b
Wenxiu1996 <wenxiu.wang@insa-lyon.fr>
2017-10-16 23:35:22 +0200

    assignment3

---
+++ Node.java
@@ -0,0 +1,16 @@
+package Node;
+
+public class Node {
+	int key;
+	int value;
+	Node left;
+	Node right;
+
+public Node(int key,int value) {
+	this.left=null;
+	this.right=null;
+	this.key=key;
+	this.value=value;
+}
+
+}
